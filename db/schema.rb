945f28dc83000d9aa58bd09ed5d81535074d7fe2
williambayliss <wmbayliss@gmail.com>
2021-05-10 19:16:38 -0400

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_205339) do
+ActiveRecord::Schema.define(version: 2021_05_10_230618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airports", force: :cascade do |t|
-    t.string "location"
+    t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
