139d5f6f01b505beab43d088a035e0986dd9ce96
williambayliss <wmbayliss@gmail.com>
2021-05-12 19:20:17 -0400

    Created flight has_many passengers through: bookings relationship

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
     belongs_to :destination_airport, inverse_of: :arriving_flights, class_name: "Airport"
     belongs_to :origin_airport, inverse_of: :departing_flights, class_name: "Airport"
+    has_many :bookings
+    has_many :passengers, through: :bookings
 end
