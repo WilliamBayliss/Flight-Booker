9fbfa8580f8f93dc3d6ebe1b16284b5270197846
williambayliss <wmbayliss@gmail.com>
2021-05-12 19:00:48 -0400

    DB migrations for bookings & passengers, added basic name/email columns to passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_230618) do
+ActiveRecord::Schema.define(version: 2021_05_12_225420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2021_05_10_230618) do
     t.string "country"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "flights", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -31,6 +36,13 @@ ActiveRecord::Schema.define(version: 2021_05_10_230618) do
     t.integer "destination_airport_id"
     t.datetime "time"
     t.string "duration"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "email"
   end
 
 end
