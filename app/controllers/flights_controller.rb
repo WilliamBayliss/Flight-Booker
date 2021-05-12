57b7877b15cae476c9fc07e01ea9f8cf6302b5bd
williambayliss <wmbayliss@gmail.com>
2021-05-12 18:57:44 -0400

    Added logic to #index that will filter @flights based on search params from form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,18 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
     @airports = Airport.all.map{ |airport| [airport.name, airport.id] }
+    @dates = Flight.pluck(:time).map{ |d| d.strftime("%d/%m/%Y") }
+
+    if params[:from_airport] && params[:to_airport]
+      @flights = Flight.where(origin_airport: params[:from_airport], destination_airport: params[:to_airport])
+    else
+      @flights = Flight.all
+    end
   end
 
   def show
     @flight = Flight.find(params[:id])
   end
+
+
 end
