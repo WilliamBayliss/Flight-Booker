79258bfce3b87e6ce1cd910d523e6bc185cd0b80
williambayliss <wmbayliss@gmail.com>
2021-05-12 22:34:34 -0400

    Created methods for bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit()
+    end
 end
