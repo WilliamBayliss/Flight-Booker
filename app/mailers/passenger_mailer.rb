8341b251361d7d119f224525b2c139e739e1ec7d
williambayliss <wmbayliss@gmail.com>
2021-06-28 17:32:37 -0400

    added to: :letter_mailer to fix bug preventing mail from opening on send

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,7 +2,6 @@ class PassengerMailer < ApplicationMailer
 
     def welcome_email
         @booking = params[:booking]
-        @url = 'http://example.com/login'
-        mail(to: @user.email, subject: 'Welcome to FlightBooker!')
+        mail(to: :letter_mailer, subject: 'Welcome to FlightBooker!')
     end 
 end
