3b890c75587a2eb8cf9f5d5338f6f55c80c6a7f8
williambayliss <wmbayliss@gmail.com>
2021-06-28 17:33:29 -0400

    Letter opener config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  LetterOpener.configure do |config|
+    # To overrider the location for message storage.
+    # Default value is `tmp/letter_opener`
+    config.location = Rails.root.join('tmp', 'my_mails')
+  end
 end
